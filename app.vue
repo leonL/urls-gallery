939b6fc0574d3e754b967272eb2d5e129b2e68a5
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2025-05-14 16:53:45 -0400

    example of using lookups for has many relationships

--- app.vue
+++ app.vue
@@ -18,6 +18,16 @@
         <p>{{ lookupStore.getLabel('geoScopes', r.geographicScopeId, 'fr') }}</p>
         <p>{{ lookupStore.getLabel('languages', r.languageId) }}</p>
         <p>{{ lookupStore.getLabel('contentTypes', r.languageId) }}</p>
+        <ul>
+          <h5>content types (english)</h5>
+          <li v-for="cTypeId in r.contentTypeIds" :key="cTypeId">
+            <p>{{ lookupStore.getLabel('contentTypes', cTypeId) }}</p>
+          </li>
+          <h5>content types (french)</h5>
+          <li v-for="cTypeId in r.contentTypeIds" :key="cTypeId">
+            <p>{{ lookupStore.getLabel('contentTypes', cTypeId, 'fr') }}</p>
+          </li>
+        </ul>
       </li>
     </ul>
   </div>
