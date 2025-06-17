86210f13f5d84846c6f9abb6fbd0420af8b20bd5
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2025-06-17 17:04:46 -0400

    compute resource facets based on resource language key

--- components/Resource.vue
+++ components/Resource.vue
@@ -1,17 +1,49 @@
 <script setup lang="ts">
   import type { Resource } from '~/stores/resources';
   import { useLookupStore } from '~/stores/lookups';
-
-  const lookupStore = useLookupStore();
-
+  
   const props = defineProps<{
     r: Resource
-  }>()
+  }>();
+
+  const { locale } = useI18n();
+  
+  const hasTranslation = computed(() => {
+    return props.r.languageId === 'both';
+  });
+
+  const rLang = computed(() => {
+    return hasTranslation.value ? locale.value : props.r.languageId as 'en' | 'fr';
+  });
+
+  const title = computed(() => {
+    return props.r.title[rLang.value];
+  });
+
+  const hasDocUrl = computed(() => {
+    const docUrl = props.r.docUrl[rLang.value];
+    return docUrl !== '';
+  });
+
+  const url = computed(() => {
+    const url = hasDocUrl.value ? props.r.docUrl[rLang.value] : props.r.webUrl[rLang.value];
+    return url;
+  });
+
+  // const lookupStore = useLookupStore();
 
 </script>
 
 <template>
-  <p>{{ r.enTitle }}</p>
+  <li class="resource">
+
+    <h1 class="title">
+      <a :href="url" target="_blank">{{ title }}</a>
+    </h1>
+  </li>
+</template>
+  
+  <!-- <p></p>
   <p>{{ r.pubYear }}</p>
   <p>{{ lookupStore.getLabel('geoScopes', r.geographicScopeId, 'fr') }}</p>
   <p>{{ lookupStore.getLabel('languages', r.languageId) }}</p>
@@ -25,5 +57,4 @@
     <li v-for="cTypeId in r.contentTypeIds" :key="cTypeId">
       <p>{{ lookupStore.getLabel('contentTypes', cTypeId, 'fr') }}</p>
     </li>
-  </ul>
-</template>
+  </ul> -->
