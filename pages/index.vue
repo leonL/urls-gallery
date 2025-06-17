58766539956c5abeee1ab35abb8d95845fce3b1a
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2025-06-16 22:02:31 -0400

    create basic file structure for layout and pages

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+  import { useResourceStore } from '../stores/resources';
+  import { useLookupStore } from '../stores/lookups';
+  
+  const resourceStore = useResourceStore();
+  const lookupStore = useLookupStore();
+  await callOnce('resources', () => resourceStore.fetch());
+  await callOnce('categories', () => lookupStore.fetch());
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ resourceStore.validCount }} Valid Resources</h1>
+    <h5>{{ resourceStore.count }} Resources</h5>
+    <ul>
+      <li v-for="r in resourceStore.valid" :key="r.id">
+        <p>{{ r.enTitle }}</p>
+        <p>{{ r.pubYear }}</p>
+        <p>{{ lookupStore.getLabel('geoScopes', r.geographicScopeId, 'fr') }}</p>
+        <p>{{ lookupStore.getLabel('languages', r.languageId) }}</p>
+        <p>{{ lookupStore.getLabel('contentTypes', r.languageId) }}</p>
+        <ul>
+          <h5>content types (english)</h5>
+          <li v-for="cTypeId in r.contentTypeIds" :key="cTypeId">
+            <p>{{ lookupStore.getLabel('contentTypes', cTypeId) }}</p>
+          </li>
+          <h5>content types (francais)</h5>
+          <li v-for="cTypeId in r.contentTypeIds" :key="cTypeId">
+            <p>{{ lookupStore.getLabel('contentTypes', cTypeId, 'fr') }}</p>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
