3add255c96286b38bbb45340741a8a4746680276
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2025-06-19 15:37:53 -0400

    resource should display notes

--- shared/api/data.ts
+++ shared/api/data.ts
@@ -8,6 +8,8 @@ interface ApiResourceRow {
     ['DOCUMENT FR']: Array<{url: string}>,
     ['LINK EN']: string,
     ['LINK FR']: string,
+    ['NOTES EN']: string,
+    ['NOTES FR']: string,
     ['AUTHOR']: string,
     ['GEOGRAPHIC SCOPE ID']: Array<string>,
     ['PUBLICATION ID']: Array<string>,
