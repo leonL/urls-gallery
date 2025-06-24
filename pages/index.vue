787fb613b2ae1e210d75f471ac4d2b23deadd66d
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2025-06-24 17:36:46 -0400

    add pagination to resource list

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,23 @@
   import { useResourceStore } from '~/stores/resources';
   
   const resourceStore = useResourceStore();
+
+  const page = ref(1);
+  const pageSize = 10;
+  const paginatedResources = computed(() => {
+    const start = (page.value - 1) * pageSize;
+    return resourceStore.valid.slice(start, start + pageSize);
+  });
 </script>
 
 <template>
   <div>
-    <h1>{{ resourceStore.validCount }} Valid Resources</h1>
-    <h5>{{ resourceStore.count }} Resources</h5>
     <ul>
-      <Resource v-for="resource in resourceStore.valid" 
-        :key="resource.id" 
-        :r="resource"
+      <Resource v-for="resource in paginatedResources" 
+      :key="resource.id" 
+      :r="resource"
       />
     </ul>
   </div>
+  <UPagination v-model:page="page" :items-per-page="pageSize" :total="resourceStore.validCount" />
 </template>
