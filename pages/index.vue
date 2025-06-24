54c61ed68427da0ba8829150b28a7eff15eab40f
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2025-06-24 19:18:29 -0400

    basic layout component breakdown

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,37 @@
 <script setup lang="ts">
-  import { useResourceStore } from '~/stores/resources';
-  
-  const resourceStore = useResourceStore();
-
-  const resourcesPerPage = 10;
-  const currentPage = ref(1);
-  const currentPageIndexRange = computed(() => {
-    const rangeEnd = (resourcesPerPage * currentPage.value);
-    const rangeStart = rangeEnd - resourcesPerPage;
-    return [rangeStart, rangeEnd];
-  });
-  const paginatedResources = computed(() => resourceStore.valid.slice(...currentPageIndexRange.value));
-  const currentPageResourceCount = computed(() => paginatedResources.value.length);
 </script>
 
 <template>
   <div>
-    <span class='pagination-state'>
-      {{ currentPageIndexRange[0] + 1 }} &#8211; {{ currentPageIndexRange[0] + currentPageResourceCount }}
-      {{ $t('of') }} {{ resourceStore.validCount }} {{ $t('results') }}
-    </span>
-    <ul>
-      <Resource v-for="resource in paginatedResources" 
-      :key="resource.id" 
-      :r="resource"
-      />
-    </ul>
+    <div class='titles'>
+      <h1 class="title">
+        {{ $t('homeTitle') }} 
+      </h1>
+      <!-- <h3 class="subtitle" v-html="$md.render(subtitle)"></h3> -->
+    </div>
+
+    <div class='gallery'>
+      <!-- // <div class="filters">
+        <IssuesFilters /> 
+        <Filters />
+      </div> -->
+
+      <div class='index'>
+        <ResourceList />
+      </div>
+    </div>
   </div>
-  <UPagination v-model:page="currentPage" :items-per-page="resourcesPerPage" :total="resourceStore.validCount" />
 </template>
+
+<style scoped>
+  .titles {
+    margin-bottom: 40px;
+  }
+
+  .title {
+    font-size: 48px;
+    font-weight: bold;
+    color: var(--primary-color);
+    margin-bottom: 10px;
+  }
+</style>
