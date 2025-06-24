8c287fabcdfb7e8aa6da8ad7846500b683e4bfb4
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2025-06-24 18:26:44 -0400

    pagination state logic and elements

--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,23 @@
   
   const resourceStore = useResourceStore();
 
-  const page = ref(1);
-  const pageSize = 10;
-  const paginatedResources = computed(() => {
-    const start = (page.value - 1) * pageSize;
-    return resourceStore.valid.slice(start, start + pageSize);
+  const resourcesPerPage = 10;
+  const currentPage = ref(1);
+  const currentPageIndexRange = computed(() => {
+    const rangeEnd = (resourcesPerPage * currentPage.value);
+    const rangeStart = rangeEnd - resourcesPerPage;
+    return [rangeStart, rangeEnd];
   });
+  const paginatedResources = computed(() => resourceStore.valid.slice(...currentPageIndexRange.value));
+  const currentPageResourceCount = computed(() => paginatedResources.value.length);
 </script>
 
 <template>
   <div>
+    <span class='pagination-state'>
+      {{ currentPageIndexRange[0] + 1 }} &#8211; {{ currentPageIndexRange[0] + currentPageResourceCount }}
+      {{ $t('of') }} {{ resourceStore.validCount }} {{ $t('results') }}
+    </span>
     <ul>
       <Resource v-for="resource in paginatedResources" 
       :key="resource.id" 
@@ -20,5 +27,5 @@
       />
     </ul>
   </div>
-  <UPagination v-model:page="page" :items-per-page="pageSize" :total="resourceStore.validCount" />
+  <UPagination v-model:page="currentPage" :items-per-page="resourcesPerPage" :total="resourceStore.validCount" />
 </template>
