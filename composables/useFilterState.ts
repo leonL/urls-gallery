f4a19e615dc3ca5cdac3b957404bf33e33bd0b02
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2025-07-23 16:41:01 -0400

    add clear all filters button

--- composables/useFilterState.ts
+++ composables/useFilterState.ts
@@ -14,6 +14,17 @@ export function useFilterState() {
 
   const currentYear = new Date().getFullYear();
 
+  const initialFilterState = {
+    issueIds: [],
+    contentTypeIds: [],
+    geographicScopeId: [],
+    languageId: "both",
+    yearPublishedRange: {
+      start: resourceStore.earliestPublicationYear,
+      end: currentYear 
+    } 
+  }
+
   const hasAnyIssues = computed(() => filter.value.issueIds.length > 0);
   const isLanguageSpecific = computed(() => filter.value.languageId !== "both");
   const hasCustomYearRange = computed(() => {
@@ -22,6 +33,14 @@ export function useFilterState() {
   });
   const hasAnyContentTypes = computed(() => filter.value.contentTypeIds.length > 0);
   const hasAnyGeographicScopes = computed(() => filter.value.geographicScopeId.length > 0);
+
+  const isActive = computed(() => {
+    return hasAnyIssues.value || 
+      isLanguageSpecific.value || 
+      hasCustomYearRange.value || 
+      hasAnyContentTypes.value || 
+      hasAnyGeographicScopes.value;
+  }); 
 
   const hasIssues = (issueIds: string[]): boolean => {
     return issueIds.some(id => filter.value.issueIds.includes(id));
@@ -54,17 +73,13 @@ export function useFilterState() {
     }
   };
 
-  const filter = useState<Filter>('resourceFilter', () => ({
-    issueIds: [],
-    contentTypeIds: [],
-    geographicScopeId: [],
-    languageId: "both",
-    yearPublishedRange: {
-      start: resourceStore.earliestPublicationYear,
-      end: currentYear 
-    } 
-  }));
+  const reset = () => {
+    filter.value = initialFilterState;
+  }
 
-  return { filter, hasAnyIssues, isLanguageSpecific, hasCustomYearRange, 
+  const filter = useState<Filter>('resourceFilter', () => (initialFilterState));
+
+
+  return { filter, isActive, hasAnyIssues, isLanguageSpecific, hasCustomYearRange, reset,
     hasAnyContentTypes, hasAnyGeographicScopes, resetByType, hasIssues, removeIssues }
 }
