0d0eaecec3368f92fd222bb17e74033c5d818869
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2025-09-09 15:11:08 -0400

    simplify sort order options

--- components/SortOrderInput.vue
+++ components/SortOrderInput.vue
@@ -2,19 +2,17 @@
   const { locale } = useI18n();
 
   const options = [
-    {value: 'published-desc', en: "date published (new to old)", fr: "date publiée (nouveau à vieux)"},
-    {value: 'published-asc', en: "date published (old to new)", fr: "date publiée (ancien au nouveau)"},
-    {value: 'added-desc', en: "date posted (new to old)", fr: "date postée (nouveau à vieux)"},
-    {value: 'added-asc', en: "date posted (old to new)", fr: "date postée (ancien au nouveau)"}
+    {value: 'added', en: "date posted", fr: "date postée"},
+    {value: 'published', en: "date published", fr: "date publiée"}
   ];
 
-  const state = useState('sortOrderState');
+  const sortBy = useState('sortByDateType');
 </script>
 
 <template>
   <span id='sort-order'>
-    <label>Order by</label>
-    <select v-model="state">
+    <label>Sort by</label>
+    <select v-model="sortBy">
       <option v-for="opt in options" :key="opt.value" :value="opt.value">
         {{ opt[locale] }}
       </option>
