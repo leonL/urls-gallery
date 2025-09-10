d35d50dc04bc510ab417e19c65ca00c8687fd519
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2025-09-10 18:07:33 -0400

    filter button styles

--- components/ResourceList.vue
+++ components/ResourceList.vue
@@ -19,6 +19,10 @@
 <template>
   <div>
     <div id="header">
+      <span id='pagination-state'>
+        {{ currentPageIndexRange[0] + 1 }} &#8211; {{ currentPageIndexRange[0] + currentPageResourceCount }}
+        {{ $t('of') }} {{ totalResourcesCount }} {{ $t('results') }}
+      </span>
       <ClientOnly>
         <UModal title="Filters" v-if="isSmallScreen">
           <button id="open-filter">
@@ -32,10 +36,6 @@
           </template>
         </UModal>
       </ClientOnly>
-      <span id='pagination-state'>
-        {{ currentPageIndexRange[0] + 1 }} &#8211; {{ currentPageIndexRange[0] + currentPageResourceCount }}
-        {{ $t('of') }} {{ totalResourcesCount }} {{ $t('results') }}
-      </span>
       <SortOrderInput />
     </div>
     <ul>
@@ -63,8 +63,6 @@
   }
   
   #pagination-state {
-    margin-right: auto;
-    margin-left: 5px;
     font-size: 18px;
     font-weight: bold;  
   }
@@ -80,18 +78,20 @@
   }
 
   button#open-filter {
+    margin-right: auto;
+    margin-left: 10px;
     padding: 3px 6px 0 6px;
     border-radius: 10%;
-    background-color: ghostwhite;
-    border: 1px solid red;
+    background-color: whitesmoke;
+    border: 1px solid dimgrey;
   }
 
   #settings-icon {
     display: inline-block;
-    width: 18px;
-    height: 18px;
+    width: 20px;
+    height: 20px;
     background-repeat: no-repeat;
     background-size: 100% 100%;
-    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='darkred' d='M3.46 5c-.21 0-.42.08-.59.21c-.44.34-.52.97-.18 1.4v.01L8 13.42v5.99l2.29 2.3c.39.39 1.03.39 1.42 0s.39-1.03 0-1.42L10 18.59v-5.86L4.27 5.39A1.01 1.01 0 0 0 3.46 5M16 12v7.88c.04.3-.06.62-.29.83a.996.996 0 0 1-1.41 0l-2.01-2.01a.99.99 0 0 1-.29-.83V12h-.03L6.21 4.62a1 1 0 0 1 .17-1.4c.19-.14.4-.22.62-.22h14c.22 0 .43.08.62.22a1 1 0 0 1 .17 1.4L16.03 12z'/%3E%3C/svg%3E");
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='red' d='M12 12v7.88c.04.3-.06.62-.29.83a.996.996 0 0 1-1.41 0L8.29 18.7a.99.99 0 0 1-.29-.83V12h-.03L2.21 4.62a1 1 0 0 1 .17-1.4c.19-.14.4-.22.62-.22h14c.22 0 .43.08.62.22a1 1 0 0 1 .17 1.4L12.03 12zm3 5h3v-3h2v3h3v2h-3v3h-2v-3h-3z'/%3E%3C/svg%3E");
   }
 </style>
